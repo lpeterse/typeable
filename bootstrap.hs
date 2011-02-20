4f0bc5d0c6ac334b1c49aaf4585fd0a9b6eeebcb
Lars Petersen <info@lars-petersen.net>
2011-02-20 16:53:25 +0000

    doesn't build. work in progress

--- bootstrap.hs
+++ bootstrap.hs
@@ -3,18 +3,18 @@ module Main where
 import Network.Curl
 
 main :: IO ()
-main  = do a <- curlGetString "http://typeable.org/type" [] 
+main  = do a <- curlGetString "http://typeable.org:5005/type" [] 
            case a of
              (CurlOK, s) -> do xs <- download $ lines s
                                mapM_ (\(n,x)->writeFile ("src/Typeable/T"++n++".hs") x) $ zip (lines s) xs
                                b  <- readFile "typeable.cabal.tpl"
                                let ns = map (\n->"                  ,Typeable.T"++n) $ lines s 
-                               writeFile "typeable.cabal" (b++(concat ns))
+                               writeFile "typeable.cabal" (b++(unlines ns))
                                print "Bootstrap successfull!"
              (x,      s) -> error $ "Can't download type listing: "++(show (x,s))
 
 download   :: [String] -> IO [String]
-download xs = do ys <- mapM (\x->curlGetString ("http://typeable.org/type/"++x++"?format=haskell") []) xs
+download xs = do ys <- mapM (\x->curlGetString ("http://typeable.org:5005/type/"++x++"?format=haskell") []) xs
                  if all ((==CurlOK) . fst) ys 
                    then return $ map snd ys
                    else (mapM_ f $ filter ((/=CurlOK) . fst . fst) (zip ys xs)) >> error ""
--- typeable.cabal.tpl
+++ typeable.cabal.tpl
@@ -50,13 +50,4 @@ Library
                   TypeableInternal.InternalTypeDefs, 
                   TypeableInternal.FormatHaskell, 
                   TypeableInternal.Context, 
-                  TypeableInternal.NamespaceParser,
-                  Typeable.Cc6ebaa9f4cdc4068894d1ffaef5a7a83,
-                  Typeable.T421496848904471ea3197f25e2a02b72,
-                  Typeable.T9e2e1e478e094a8abe5507f8574ac91f,
-                  Typeable.Cb5ba7ec44dbb4236826c6ef6bc4837e4, 
-                  Typeable.Taf20e1db8f0d414f90625b1521e41378,
-                  -- UTC
-                  Typeable.Tc1b1f6c722c2436fab3180146520814e,
-                  -- Time
-                  Typeable.T606f253533d3420da3465afae341d598
+                  TypeableInternal.NamespaceParser
